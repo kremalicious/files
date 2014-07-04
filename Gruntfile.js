93820c84184c8367dce2e5c35951530d6f645409
Matthias Kretschmann <m@kretschmann.io>
2014-07-04 17:36:31 +0200

    gruntify that

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt){
+    'use strict';
+
+    // banner
+    grunt.log.writeln("");
+    grunt.log.writeln("   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    grunt.log.writeln("");
+    grunt.log.writeln("      (o) Just what do you think you're doing, Matthias?    ");
+    grunt.log.writeln("");
+    grunt.log.writeln("   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    grunt.log.writeln("");
+
+    // Grunt config
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // less
+        less: {
+            production: {
+                options: {
+                    cleancss: true,
+                    ieCompat: false
+                },
+                files: {
+                    'swtndx/assets/kremalicious.css' : 'swtndx/assets/kremalicious.less'
+                }
+            }
+        },
+
+        // watch
+        watch: {
+            options: {
+                livereload: true
+            },
+            less: {
+                files: ['swtndx/assets/*.less'],
+                tasks: ['less']
+            }
+        },
+
+
+        // rsync stuff around
+        rsync: {
+            options: {
+                recursive: true
+            },
+            // deployment
+            deploy: {
+                options: {
+                    syncDest: true,
+                    src: '.',
+                    dest: 'domains/files.kremalicious.com/html',
+                    host: 'kremalicious',
+                    ssh: true,
+                    compareMode: 'checksum',
+                    args: ['--verbose']
+                }
+            }
+        }
+
+    });
+
+    // Load NPM Tasks, smart code stolen from @bluemaex <https://github.com/bluemaex>
+    require('fs').readdirSync('node_modules').filter(function (file) {
+        return file && file.indexOf('grunt-') > -1;
+    }).forEach(function (file) {
+        grunt.loadNpmTasks(file);
+    });
+
+    // Default Task
+    grunt.registerTask('default', [
+        'less',
+        'watch'
+    ]);
+
+    // Deploy
+    grunt.registerTask('deploy', [
+        'rsync:deploy'
+    ]);
+
+};
